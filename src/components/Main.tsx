d7fcb955b47214091ef31fc2c9c95f17ad3d4763
Partha-sarathi-07 <sarathicharu07@gmail.com>
2025-04-24 08:12:09 +0530

    added some ingredient list

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,31 @@
+import { FormEvent } from 'react';
 import '../styles/main.css'
 export default function Main() {
+    const ingredients: string[] = ['Chicken', "Oregano", "Tomatoes"];
+    const ingredientList = ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>);
+
+    function handleOnSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient")?.toString();
+        ingredients.push(newIngredient!);
+        console.log(newIngredient);
+        console.log(ingredients);
+    }
     return(
         <main>
-            <form>
+            <form onSubmit={handleOnSubmit}>
                 <input 
                     type='text' 
                     aria-label='Add ingredient'
                     placeholder='e.g. Oregano'
+                    name='ingredient'
                 />
                 <button>+ Add ingredient</button>
             </form>
+            <ul>
+                {ingredientList}
+            </ul>
         </main>
     )
 }
